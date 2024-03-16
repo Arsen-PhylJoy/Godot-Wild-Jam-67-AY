a095dc042b9c57e755ea2460f51e08c980369e18
Arsen-PhylJoy <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-03-16 20:21:59 +0300

    combo_system enhacement
    Now multiplicator scale fades away If you AFK
    Now laber "You are awesome" doesn't emerge every time you gain full x16 scale

--- scenes/ui/combo_labels.gd
+++ scenes/ui/combo_labels.gd
@@ -16,7 +16,10 @@ var _points_to_next_x: int = 100
 func _ready() -> void:
 	if _x_progess_bar.value_changed.connect(_update_x_progress_bar): printerr("Fail: ",get_stack())
 	if _gradient_update_timer.timeout.connect(_random_change_gradient_offsets): printerr("Fail: ",get_stack())
-			
+
+func _physics_process(delta: float) -> void:
+	_x_progess_bar.value-=0.125
+
 func _calculate(reward: int)->void:
 	var result_reward:int = reward * _multiplicator
 	_current_points+=result_reward
@@ -29,7 +32,7 @@ func _calculate(reward: int)->void:
 		_lower_multiplicator()
 
 func _update_x_progress_bar(new_value: float)->void:
-	if(new_value>=100):
+	if(new_value>=100 and _multiplicator != _MULTIPLICATOR.X16):
 		_increase_multiplicator()
 		_x_progess_bar.value = 25
 	elif(new_value<=0):
