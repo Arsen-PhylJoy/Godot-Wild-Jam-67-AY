7dc9066208f83383591904bf6cb41685418224b9
Arsen-PhylJoy <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-03-17 21:17:51 +0300

    end game screen

--- levels/game/prehistory.gd
+++ levels/game/prehistory.gd
@@ -1,1 +1,10 @@
 extends Node
+
+@onready var _menu_button: Button = %Continue
+
+func _ready() -> void:
+	if _menu_button.connect("pressed", _on_pressed_continue_button): printerr("Fail: ",get_stack()) 
+
+
+func _on_pressed_continue_button()->void:
+	LevelManager.load_level("res://levels/game/main_menu.tscn")
