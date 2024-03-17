1a8ab0049f5f85bfffcd8f6f0ef2a38d1ebab445
Arsen-PhylJoy <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-03-17 10:05:44 +0300

    Add win window and fix collisions

---
+++ levels/game/win_window.gd
@@ -0,0 +1,10 @@
+extends Node
+
+@onready var _exit_button: Button = %Exit
+
+func _ready() -> void:
+	if _exit_button.pressed.connect(_exit): printerr("Fail: ",get_stack())
+
+
+func _exit()->void:
+	LevelManager.load_level("res://levels/game/main_menu.tscn")
--- scenes/ui/combo_labels.gd
+++ scenes/ui/combo_labels.gd
@@ -17,8 +17,8 @@ func _ready() -> void:
 	if _x_progess_bar.value_changed.connect(_update_x_progress_bar): printerr("Fail: ",get_stack())
 	if _gradient_update_timer.timeout.connect(_random_change_gradient_offsets): printerr("Fail: ",get_stack())
 
-func _physics_process(delta: float) -> void:
-	_x_progess_bar.value-=0.125
+func _physics_process(_delta: float) -> void:
+	_x_progess_bar.value-=0.1
 
 func _calculate(reward: int)->void:
 	var result_reward:int = reward * _multiplicator
@@ -34,7 +34,7 @@ func _calculate(reward: int)->void:
 func _update_x_progress_bar(new_value: float)->void:
 	if(new_value>=100 and _multiplicator != _MULTIPLICATOR.X16):
 		_increase_multiplicator()
-		_x_progess_bar.value = 25
+		_x_progess_bar.value = 50
 	elif(new_value<=0):
 		_lower_multiplicator()
 		
